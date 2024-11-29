7f5ea0dfbade0e4728bfb062e33db6a9c04280c5
MrKiddie <NajwaRafifahKhairunnisa@gmail.com>
2024-11-30 02:20:41 +0700

    Add files via upload

---
+++ osint.sh
@@ -0,0 +1,364 @@
+#!/bin/bash
+
+# Variabel warna
+CYAN='\033[0;36m'
+RED='\033[0;31m'
+YELLOW='\033[0;33m'
+RESET='\033[0m'
+
+# Fungsi untuk menampilkan banner ASCII
+ascii_art() {
+  echo   
+  echo -e "${RED}"
+  echo "       ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣀⣀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀"
+  echo "       ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡰⠚⠉⠀⠀⠉⠑⢦⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀"
+  echo "       ⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⠞⠀⠀⠀⠀⠀⠀⠀⠀⠱⡄⠀⠀⠀⠀⠀⠀⠀⠀⠀"
+  echo "       ⠀⠀⠀⠀⠀⠀⠀⠀⢀⠏⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠹⡀⠀⠀⠀⠀⠀⠀⠀⠀"
+  echo "       ⠀⠀⠀⠀⠀⠀⠀⠀⡜⠀⠀⠀⠀⠀⣀⣀⠀⠀⠀⠀⠀⢣⠀⠀⠀⠀⠀⠀⠀⠀"
+  echo "       ⠀⠀⠀⠀⠀⠀⠀⠀⡇⠀⣠⠔⠋⠉⣩⣍⠉⠙⠢⣄⠀⢸⠀⠀⠀⠀⠀⠀⠀⠀"
+  echo "       ⠀⠀⠀⠀⠀⠀⠀⠀⢧⡜⢏⠓⠒⠚⠁⠈⠑⠒⠚⣹⢳⡸⠀⠀⠀⠀⠀⠀⠀⠀"
+  echo "       ⠀⠀⠀⠀⠀⠀⠀⠀⠘⣆⠸⡄⠀⠀⠀⠀⠀⠀⢠⠇⣰⠃⠀⠀⠀⠀⠀⠀⠀⠀"
+  echo "       ⠀⠀⠀⠀⠀⠀⢀⡴⠚⠉⢣⡙⢦⡀⠀⠀⢀⡰⢋⡜⠉⠓⠦⣀⠀⠀⠀⠀⠀⠀"
+  echo "       ⠀⠀⠀⠀⠀⡴⠁⢀⣀⣀⣀⣙⣦⣉⣉⣋⣉⣴⣋⣀⣀⣀⡀⠈⢧⠀⠀⠀⠀⠀"
+  echo "       ⠀⠀⠀⠀⡸⠁⠀⢸⠀⠀⠀⠀⢀⣔⡛⠛⡲⡀⠀⠀⠀⠀⡇⠀⠈⢇⠀⠀⠀⠀"
+  echo "       ⠀⠀⠀⢠⠇⠀⠀⠸⡀⠀⠀⠀⠸⣼⠽⠯⢧⠇⠀⠀⠀⠀⡇⠀⠀⠘⡆⠀⠀⠀"
+  echo "       ⠀⠀⠀⣸⠀⠀⠀⠀⡇⠀⠀⠀⠳⢼⡦⢴⡯⠞⠀⠀⠀⢰⠀⠀⠀⠀⢧⠀⠀⠀"
+  echo "       ⠀⠀⠀⢻⠀⠀⠀⠀⡇⠀⠀⠀⢀⡤⠚⠛⢦⣀⠀⠀⠀⢸⠀⠀⠀⠀⡼⠀⠀⠀"
+  echo "       ⠀⠀⠀⠈⠳⠤⠤⣖⣓⣒⣒⣒⣓⣒⣒⣒⣒⣚⣒⣒⣒⣚⣲⠤⠤⠖⠁⠀⠀⠀"
+  echo "      ⣀⣀⣀⣀⣀⣀⣀⣀⣀⣀⣀⣀⣀⣀⣀⣀⣀⣀⣀⣀⣀⣀⣀⣀⣀⣀⣀⣀⣀⣀⣀⣀"
+  echo "      ⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿"
+  echo -e "${RESET}"
+
+
+  echo -e "${CYAN}==============================================${RESET}"
+  echo -e "${RED}     [*] WARNING: USE WITH CAUTION! [*]       ${RESET}"
+  echo -e "${CYAN}==============================================${RESET}"
+  echo -e "${MAGENTA}      ⚡ SYSTEM INTEGRITY: ACTIVE ⚡        ${RESET}"
+  echo -e "${CYAN}==============================================${RESET}"
+  echo -e "${RED}     ⚠️  Gunakan alat ini dengan Bijak ⚠️${RESET}"
+  echo -e "${CYAN}==============================================${RESET}"
+  echo -e "${YELLOW}       * OSINT Investigation Tool *         ${RESET}"
+  echo -e "${CYAN}==============================================${RESET}"
+  echo -e "${MAGENTA}#Author : Aldi (Human Cyber Security)      ${RESET}"
+  echo -e "${MAGENTA}#Support: Hackerroom.id                    ${RESET}"
+  echo -e "${CYAN}----------------------------------------------${RESET}"
+  echo -e "${CYAN}                Menu Pilihan:                 ${RESET}"
+
+  echo
+  echo -e "======================================="
+  echo -e "1. Search informasi nomor pembayaran"
+  echo -e "2. Search informasi nama Mahasiswa"
+  echo -e "3. Search informasi Jejak Email"
+  echo -e "4. Search nama nomor Rekening Bank"
+  echo -e "5. Search informasi Identitas Nik Ktp & Bpjs"
+  echo -e "6. Keluar"
+  echo -e "======================================="
+  echo
+  echo -n "Pilih menu informasi pencarian: "
+}
+
+# Fungsi untuk mencari informasi e-wallet
+search_wallet_info() {
+    data='[{"kodeBank":"DANA","namaBank":"DANA"},{"kodeBank":"GOPAY","namaBank":"GOPAY"},{"kodeBank":"GOPAYDRIVER","namaBank":"GOPAY DRIVER"},{"kodeBank":"KASPRO","namaBank":"KASPRO"},{"kodeBank":"LINKAJA","namaBank":"LINKAJA"},{"kodeBank":"OVO","namaBank":"OVO"},{"kodeBank":"SHOPEEPAY","namaBank":"SHOPEEPAY"}]'
+
+    read -p "Masukkan nomor telepon (08xxxxxxx): " nomor
+
+    if [[ ! "$nomor" =~ ^08[0-9]{8,11}$ ]]; then
+        echo "Nomor telepon tidak valid. Pastikan formatnya benar (08xxxxxxx)."
+        exit 1
+    fi
+
+    echo -e "\n${CYAN}Mencari informasi e-wallet. Mohon tunggu.${RESET}"
+    for i in {1..3}; do
+        echo -ne "${CYAN}.\033[0m"
+        sleep 1
+    done
+    echo -e "\n${CYAN}Proses sedang berlangsung.${RESET}"
+    sleep 5
+
+    for item in $(echo "$data" | jq -r '.[] | @base64'); do
+        _jq() {
+            echo ${item} | base64 --decode | jq -r ${1}
+        }
+
+        kodeBank=$(_jq '.kodeBank')
+        namaBank=$(_jq '.namaBank')
+
+        response=$(curl -s "https://api-rekening.lfourr.com/getEwalletAccount?bankCode=$kodeBank&accountNumber=$nomor")
+
+        status=$(echo "$response" | jq -r '.status')
+        if [ "$status" == "true" ]; then
+            accountName=$(echo "$response" | jq -r '.data.accountname // empty')
+            echo -e "\nData untuk $namaBank:"
+            echo -e "kodeBank: $kodeBank"
+            echo -e "${RED}accountName: $accountName${RESET}"
+        else
+            echo -e "\nData untuk $namaBank:"
+            echo -e "kodeBank: $kodeBank"
+            echo -e "accountName: Tidak ditemukan"
+        fi
+        echo -e "${YELLOW}==========================${RESET}"
+    done
+}
+
+# Fungsi untuk mencari mahasiswa
+search_mahasiswa_info() {
+    read -p "Masukkan nama mahasiswa: " nama_mahasiswa
+    read -p "Masukkan nama universitas: " nama_universitas
+
+    result=$(curl -s -X GET "https://pddikti.kemdikbud.go.id/api/pencarian/mhs/${nama_mahasiswa// /%20}" \
+    -H "Content-Type: application/json" \
+    -H "x-api-key: 3ed297db-db1c-4266-8bf4-a89f21c01317")
+
+    if echo "$result" | jq -e '. | length > 0' >/dev/null; then
+        mahasiswa_cocok=$(echo "$result" | jq -r ".[] | select(.nama | test(\"$nama_mahasiswa\"; \"i\")) | select(.nama_pt | test(\"$nama_universitas\"; \"i\"))")
+
+        if [ -n "$mahasiswa_cocok" ]; then
+            echo "Mahasiswa yang ditemukan:"
+            echo "$mahasiswa_cocok" | jq '. | {nama: .nama, id: .id, universitas: .nama_pt}'
+            
+            read -p "Masukkan ID mahasiswa untuk melihat detail: " id_mahasiswa
+            
+            detail_result=$(curl -s -X GET "https://pddikti.kemdikbud.go.id/api/detail/mhs/$id_mahasiswa" \
+            -H "Content-Type: application/json" \
+            -H "x-api-key: 3ed297db-db1c-4266-8bf4-a89f21c01317")
+            
+            echo "$detail_result" | jq
+        else
+            echo "Mahasiswa dengan nama '$nama_mahasiswa' dari universitas '$nama_universitas' tidak ditemukan."
+        fi
+    else
+        echo "Tidak ada hasil untuk nama '$nama_mahasiswa'."
+    fi
+}
+
+
+# Fungsi untuk mengekstrak data dari API berdasarkan email
+search_jejak_info_email() {
+    # Meminta email untuk diperiksa
+    read -p "Masukkan email: " mail
+
+    # Validasi email
+    if [[ ! "$mail" =~ ^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$ ]]; then
+        echo -e "${RED}Email tidak valid. Pastikan formatnya benar.${RESET}"
+        exit 1
+    fi
+
+    # Mengirim permintaan GET ke API menggunakan Curl
+    response=$(curl -s --request GET \
+        --url "https://google-data.p.rapidapi.com/email/$mail" \
+        --header 'X-RapidAPI-Host: google-data.p.rapidapi.com' \
+        --header 'X-RapidAPI-Key: 66c542f90cmsh73293ebe691cab8p19db74jsn5eca3a24bdb4')
+
+    # Menampilkan data yang difilter menggunakan jq
+    echo -e "${CYAN}Data untuk email $mail:${RESET}"
+
+    # Mengekstrak dan menampilkan data sensitif dari JSON
+    profile_photo=$(echo "$response" | jq -r '.PROFILE_CONTAINER.profile.profilePhotos.PROFILE.url // empty')
+    email=$(echo "$response" | jq -r '.PROFILE_CONTAINER.profile.emails.PROFILE.value // empty')
+    user_types=$(echo "$response" | jq -r '.PROFILE_CONTAINER.profile.profileInfos.PROFILE.userTypes[] // empty')
+    person_id=$(echo "$response" | jq -r '.PROFILE_CONTAINER.profile.personId // empty')
+    last_updated=$(echo "$response" | jq -r '.PROFILE_CONTAINER.profile.sourceIds.PROFILE.lastUpdated // empty')
+
+    echo -e "${BOLD}Profile Photo URL:${RESET} $profile_photo"
+    echo -e "${BOLD}Email:${RESET} $email"
+    echo -e "${BOLD}User Types:${RESET} $user_types"
+    echo -e "${BOLD}Person ID:${RESET} $person_id"
+    echo -e "${BOLD}Last Updated:${RESET} $last_updated"
+
+    # Menampilkan informasi dari review pertama jika ada
+    reviews=$(echo "$response" | jq -r '.PROFILE_CONTAINER.maps.reviews[]? | "\(.location.name) - \(.location.address) - Rating: \(.rating) - Comment: \(.comment) - Date: \(.date)"')
+
+    if [ -z "$reviews" ]; then
+        echo -e "${RED}Tidak ada ulasan yang tersedia.${RESET}"
+    else
+        echo -e "${BOLD}Reviews:${RESET}"
+        echo "$reviews" | while IFS= read -r review; do
+            echo -e "$review"
+        done
+    fi
+}
+
+
+# Fungsi search_Rek_info (untuk opsi nomor 4)
+search_Rek_info() {
+    echo "Memulai pengecekan rekening bank..."
+    get_banks
+    echo "------------------------------------"
+    check_account
+}
+
+# URL API untuk daftar bank dan pengecekan akun
+BANK_LIST_URL="https://api-rekening.lfourr.com/listBank"
+CHECK_ACCOUNT_URL="https://api-rekening.lfourr.com/getBankAccount"
+
+# Fungsi untuk mengambil daftar bank dari API dan membuat menu
+get_banks() {
+    echo "Mengambil daftar bank..."
+
+    # Mendapatkan respons API
+    response=$(curl -s "$BANK_LIST_URL")
+
+    # Cek apakah response kosong
+    if [ -z "$response" ]; then
+        echo '{"error": "Gagal mengambil daftar bank. Respons kosong."}'
+        exit 1
+    fi
+
+    # Menggunakan jq untuk mengekstrak daftar bank dan membuat menu
+    banks=$(echo "$response" | jq -r '.data[]')
+
+    if [ -z "$banks" ]; then
+        echo '{"error": "Gagal mengekstrak daftar bank dari respons JSON."}'
+        exit 1
+    fi
+
+    # Menampilkan daftar bank dalam format JSON
+    echo "$banks" | jq .
+    
+    # Menyusun pilihan bank berdasarkan kode bank
+    echo ""
+    echo "Pilih kode bank dari daftar di atas (misalnya: 014 untuk BCA):"
+    echo -n "masukan kode bank yang sesuai :"
+    read bank_code
+
+    # Cek apakah kode bank valid
+    if [[ ! "$banks" =~ "$bank_code" ]]; then
+        echo '{"error": "Kode bank tidak valid. Harap pilih dari daftar."}'
+        exit 1
+    fi
+
+    echo '{"selected_bank": "'$bank_code'"}'
+}
+
+# Fungsi untuk memeriksa akun bank
+check_account() {
+    echo "Masukkan nomor akun:"
+    read account_number
+
+    # Memanggil API untuk memeriksa akun bank
+    response=$(curl -s "${CHECK_ACCOUNT_URL}?bankCode=${bank_code}&accountNumber=${account_number}")
+
+    # Cek apakah response kosong
+    if [ -z "$response" ]; then
+        echo '{"error": "Gagal mendapatkan data akun. Respons kosong."}'
+        exit 1
+    fi
+
+    # Memeriksa apakah akun valid atau tidak dengan mengecek status dari API
+    account_status=$(echo "$response" | jq -r '.status')
+
+    # Jika respons statusnya true, berarti akun ditemukan
+    if [ "$account_status" == "true" ]; then
+        # Mengambil data dari respons JSON dan menampilkannya dalam format yang rapi
+        echo "$response" | jq .
+    else
+        echo '{"error": "Akun tidak ditemukan atau error terjadi."}'
+    fi
+}
+
+
+# Fungsi search_Nik_info (untuk opsi nomor 5)
+search_Nik_info() {
+    echo "Memulai pengecekan NIK KTP..."
+    check_ktp
+}
+
+# Fungsi untuk mengambil data Nik
+check_ktp() {
+    echo
+    # Meminta input NIK dari pengguna
+    read -p "Masukkan NIK: " nik
+
+    # Menjalankan perintah nik-parse dengan NIK yang dimasukkan
+    nik-parse --n "$nik"
+
+    # Mengecek apakah NIK valid atau tidak (dianggap valid jika nik-parse tidak mengeluarkan error)
+    if [ $? -eq 0 ]; then
+        echo "NIK valid. Melanjutkan pengecekan BPJS..."
+        check_bpjs "$nik"
+    else
+        echo "NIK tidak valid. Mohon coba lagi dengan NIK yang benar."
+    fi
+}
+
+# Fungsi untuk mengecek BPJS berdasarkan NIK
+check_bpjs() {
+    nik=$1
+    url="http://simrs.belitung.go.id:3000/api/wsvclaim/pesertaNik?nik=$nik"
+
+    # Mengambil data dari API menggunakan curl dan memeriksa status HTTP
+    response=$(curl -s -w "%{http_code}" -o /tmp/response.txt "$url")
+    
+    # Cek apakah status HTTP 200 (OK)
+    if [ "$response" -eq 200 ]; then
+        # Baca respons dari file sementara
+        response_content=$(cat /tmp/response.txt)
+
+        # Menampilkan respons API dalam format vertikal menggunakan jq
+        echo "$response_content" | jq .
+
+        # Memeriksa apakah respons mengandung data BPJS
+        if echo "$response_content" | jq -e '.data' > /dev/null; then
+            echo "NIK $nik terdaftar di BPJS."
+        else
+            echo
+        fi
+    else
+        echo "Terjadi kesalahan saat mengakses API. Status HTTP: $response"
+    fi
+}
+
+
+# Fungsi untuk menampilkan menu lagi setelah aksi selesai
+ask_to_continue() {
+  echo -n "Apakah Anda ingin mencari data lagi? (y/n): "
+  read answer
+  if [[ "$answer" == "y" || "$answer" == "Y" ]]; then
+    main_menu
+  else
+    echo "Terima kasih telah menggunakan alat ini. Program selesai."
+    exit 0
+  fi
+}
+
+
+# Tambahkan fungsi utama agar program berulang
+main_menu() {
+ascii_art
+read pilihan
+
+case $pilihan in
+  1)
+    search_wallet_info
+    ask_to_continue
+    ;;
+  2)
+    search_mahasiswa_info
+    ask_to_continue
+    ;;
+  3)
+    search_jejak_info_email
+    ask_to_continue
+    ;;
+  4)
+    search_Rek_info
+    ask_to_continue
+    ;;
+  5)
+    search_Nik_info
+    ask_to_continue
+    ;;
+  6)
+    echo "Terima kasih telah menggunakan program ini. Sampai jumpa!"
+    exit 0
+    ;;
+  *)
+    echo "Pilihan tidak valid. Silakan pilih menu."
+    ask_to_continue
+    ;;
+esac
+}
+
+# Memulai menu utama
+main_menu
